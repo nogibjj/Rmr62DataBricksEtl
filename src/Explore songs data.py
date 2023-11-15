3581312f39a9a41f9587ff414ccdfd64f1d7c438
Rakeen Rouf <36940292+rmr327@users.noreply.github.com>
2023-11-15 13:58:45 -0500

    Initial commit

---
+++ src/Explore songs data.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# MAGIC %fs ls "/databricks-datasets/songs/data-001"
+
+# COMMAND ----------
+
+# MAGIC %fs head --maxBytes=10000 "/databricks-datasets/songs/README.md"
+
+# COMMAND ----------
+
+# MAGIC %fs ls "/databricks-datasets/songs/data-001"
+
+# COMMAND ----------
+
+# MAGIC %fs head --maxBytes=10000 "/databricks-datasets/songs/data-001/part-00000"
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("Read CSV").getOrCreate()
+df = spark.read.format('csv').option("sep", "\t").load('dbfs:/databricks-datasets/songs/data-001/part-00000')
+df.display()
